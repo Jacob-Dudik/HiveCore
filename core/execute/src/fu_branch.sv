0f052bc55678202e25c5b63a3aa91b14444bb0c1
Jacob Dudik <jdudik03@gmail.com>
2026-09-06 14:15:30 -0400

    Fix branch enums and taken

--- core/execute/src/fu_branch.sv
+++ core/execute/src/fu_branch.sv
@@ -1,12 +1,10 @@
 /**************************************************************************************************
 * Module Name    : fu_branch
 * Author         : Jacob Dudik
-* Creation Date  : 08/16/2026
-* Last edit Date : 08/16/2026
-* Description    : Branch Functional Unit:
-*                  Computes branch targets and branch conditions for PC related instructions.
+* Creation Date  : 09/05/2026
+* Last edit Date : 09/05/2026
+* Description    : Branch functional unit. Calculates branch outcomes and target PCs.
 **************************************************************************************************/
-
 
 `ifndef FU_BRANCH
 `define FU_BRANCH
@@ -14,10 +12,7 @@ module fu_branch
   import core_pkg::*;
   import interface_struct_pkg::*;
   (
-    // ========= Inputs ==========
     input  decode_execute_if_t dec_ex_i,
-
-    // ========= Outputs =========
     output fu_branch_out_t     fu_branch_out_o
   );
 
@@ -25,60 +20,64 @@ module fu_branch
   //         Internal Signals
   // ============================================
   logic [63:0] imm_sext;
-
-  // Sign extension of standard 20-bit immediate to 64 bits
-  assign imm_sext   = {{44{dec_ex_i.imm[19]}}, dec_ex_i.imm};
-
+  logic        branch_taken;
+  
   // ============================================
   //         Branch Execution Logic
   // ============================================
   always_comb begin
-    // Valid when input functional unit matches BRANCH
+    // Default valid checking
     fu_branch_out_o.valid     = (dec_ex_i.fu == FU_BRANCH);
     fu_branch_out_o.target_pc = '0;
-    fu_branch_out_o.taken     = '0;
+    branch_taken              = 1'b0;
+
+    // Immediate sign extension for branch target calculation
+    imm_sext = { {(64-20){dec_ex_i.imm[19]}}, dec_ex_i.imm };
 
     case (branch_uOP_e'(dec_ex_i.uOP))
       BEQ: begin
         fu_branch_out_o.target_pc = dec_ex_i.pc + imm_sext;
-        fu_branch_out_o.taken     = (dec_ex_i.rs1_data == dec_ex_i.rs2_data);
+        branch_taken              = (dec_ex_i.rs1_data == dec_ex_i.rs2_data);
       end
 
       BNE: begin
         fu_branch_out_o.target_pc = dec_ex_i.pc + imm_sext;
-        fu_branch_out_o.taken     = (dec_ex_i.rs1_data != dec_ex_i.rs2_data);
+        branch_taken              = (dec_ex_i.rs1_data != dec_ex_i.rs2_data);
       end
 
       BLT: begin
         fu_branch_out_o.target_pc = dec_ex_i.pc + imm_sext;
-        fu_branch_out_o.taken     = ($signed(dec_ex_i.rs1_data) < $signed(dec_ex_i.rs2_data));
+        branch_taken              = ($signed(dec_ex_i.rs1_data) < $signed(dec_ex_i.rs2_data));
       end
 
       BGE: begin
         fu_branch_out_o.target_pc = dec_ex_i.pc + imm_sext;
-        fu_branch_out_o.taken     = ($signed(dec_ex_i.rs1_data) >= $signed(dec_ex_i.rs2_data));
+        branch_taken              = ($signed(dec_ex_i.rs1_data) >= $signed(dec_ex_i.rs2_data));
       end
 
       BLTU: begin
         fu_branch_out_o.target_pc = dec_ex_i.pc + imm_sext;
-        fu_branch_out_o.taken     = (dec_ex_i.rs1_data < dec_ex_i.rs2_data);
+        branch_taken              = (dec_ex_i.rs1_data < dec_ex_i.rs2_data);
       end
 
       BGEU: begin
         fu_branch_out_o.target_pc = dec_ex_i.pc + imm_sext;
-        fu_branch_out_o.taken     = (dec_ex_i.rs1_data >= dec_ex_i.rs2_data);
+        branch_taken              = (dec_ex_i.rs1_data >= dec_ex_i.rs2_data);
       end
 
-      JAL: begin
+      BR_JAL: begin
         fu_branch_out_o.target_pc = dec_ex_i.pc + imm_sext;
-        fu_branch_out_o.taken     = 1'b1;
+        branch_taken              = 1'b1;
       end
 
-      JALR: begin
+      BR_JALR: begin
         fu_branch_out_o.target_pc = (dec_ex_i.rs1_data + imm_sext) & ~64'b1; // JALR result must be even
-        fu_branch_out_o.taken     = 1'b1;
+        branch_taken              = 1'b1;
       end
     endcase
+    
+    // Assert taken if this was a valid and taken branch
+    fu_branch_out_o.taken = branch_taken & fu_branch_out_o.valid;
   end
 
 endmodule
